7cd3d909ae98ec25380a0721cc3f5f11fff3290e
bracheopterix <satoru-kun@mail.ru>
2021-12-07 23:35:19 +0300

    started lottery page

---
+++ scripts/lottery.js
@@ -0,0 +1,10 @@
+let ticketNumber = document.getElementById('ticket-number');
+console.log(ticketNumber);
+
+let randomNumber =0;
+randomNumber= getRandom(randomNumber);
+
+
+function getRandom() {
+    return Math.random();
+  }
